68e29d8d204f72405a989fc88da62d75e909a692
aasin729 <115336608+aasin729@users.noreply.github.com>
2023-02-06 16:17:42 +0900

    ver0.6

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$('.slide-group').slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    arrows:true,
+    dots:true,
+    prevArrow:'<button class="slick-arrow slick-prev"><i class="fa-solid fa-angles-left"></i></button>',
+    nextArrow:'<button class="slick-arrow slick-next"><i class="fa-solid fa-angles-right"></i></button>',
+    
+})
