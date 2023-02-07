200061cdd46a314e6a190366b4b4fdad133db314
aasin729 <115336608+aasin729@users.noreply.github.com>
2023-02-07 13:35:00 +0900

    ver0.8

--- js/index.js
+++ js/index.js
@@ -7,3 +7,15 @@ $('.slide-group').slick({
     nextArrow:'<button class="slick-arrow slick-next"><i class="fa-solid fa-angles-right"></i></button>',
     
 })
+
+
+// 슬라이드 화면 정지,시작 버튼 효과 
+$('.article1 .plpa i').on('click', function(){
+    if($(this).hasClass('fa-pause')){
+        $('.article1 .slide_group').slick('slickPause')
+        $(this).removeClass('fa-pause').addClass('fa-play')
+    }else{
+        $('.article1 .slide_group').slick('slickPlay')
+        $(this).removeClass('fa-play').addClass('fa-pause')
+    }
+})
