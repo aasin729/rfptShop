c5d4744bd94675b238b580e4412e195e9124fd80
aasin729 <115336608+aasin729@users.noreply.github.com>
2023-02-09 17:15:40 +0900

    ver11.0

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $('.slide-group').slick({
     dots:true,
     prevArrow:'<button class="slick-arrow slick-prev"><i class="fa-solid fa-angles-left"></i></button>',
     nextArrow:'<button class="slick-arrow slick-next"><i class="fa-solid fa-angles-right"></i></button>',
-    
+
 })
 
 
