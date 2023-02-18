4283096cc51a7e6a372967bf3108ba76a39af36c
aasin729 <115336608+aasin729@users.noreply.github.com>
2023-02-18 11:11:30 +0900

    ver.24

--- js/common.js
+++ js/common.js
@@ -35,22 +35,8 @@ $(window).on('resize', function(){
     }     
 })
 
-// 인트로화면 0%~ 100% 이후에 메인화면 넘어가기 
+
 $(window).on('load', function(){
-
-    let count = 0; 
-    let timer = setInterval(add, 25)
-
-    function add(){
-        count++
-        if(count>=100) { 
-            clearInterval(timer) 
-            $('.introAni').delay(500).fadeOut()
-
-        }
-        $('.introAni div').eq(1).text(count+'%')
-    }
-
 
     // 새로고침하면 현재 위치한 스크롤바 위치를 맨위로 
     $('html').animate({
@@ -58,36 +44,4 @@ $(window).on('load', function(){
     }, 100)
 
 })
-
-
-
-
-//==========헤더 메뉴 하단메뉴 부분(공부목적)=============================
-
-// $('#header .open').on('click', function(){
-//     $(this).next().stop().slideToggle()
-// })
-
-// $('#nav .depth1 > li').on('mouseover mouseout', function(){
-//     if ( $('html').hasClass('pc') ) {
-//         $(this).find('.depth2').stop().slideToggle()
-//     }
-// })
-
-//======================================================================
-
-// $('#nav .depth1 > li > button').on('click', function(){
-//     if ( $('html').hasClass('mobile') ) {
-//         if ( $(this).find('i').hasClass('fa-chevron-down') ) {
-//             $(this).next().stop().slideDown()
-//             $(this).find('i').removeClass('fa-chevron-down')
-//             .addClass('fa-chevron-up')
-//         } else {
-//             $(this).next().stop().slideUp()
-//             $(this).find('i').removeClass('fa-chevron-up')
-//             .addClass('fa-chevron-down')
-//         }
-//         return false
-//     }
-// })
 
--- js/index.js
+++ js/index.js
@@ -9,17 +9,6 @@ $('.slide-group').slick({
 })
 
 
-// 슬라이드 화면 정지,시작 버튼 효과 
-$('.article1 .plpa i').on('click', function(){
-    if($(this).hasClass('fa-pause')){
-        $('.article1 .slide_group').slick('slickPause')
-        $(this).removeClass('fa-pause').addClass('fa-play')
-    }else{
-        $('.article1 .slide_group').slick('slickPlay')
-        $(this).removeClass('fa-play').addClass('fa-pause')
-    }
-})
-
 
 //스크롤 이벤트 
 
